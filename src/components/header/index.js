96f324201b351506fe822eaa3ee70017ba3a8463
krakenav9 <francisconavarroflores@gmail.com>
2022-11-26 21:14:16 -0300

    app game adivina el numero react native

---
+++ src/components/header/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import {View, Text } from "react-native";
+import { styles } from "./styles";
+
+const Header = () => {
+    return (
+    <View style={styles.container}>
+        <Text>Header</Text>
+    </View>
+
+    );
+
+}
+
+export default Header;
